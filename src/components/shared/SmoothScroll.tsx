08352b8c8b01e9ff6ba080abd947a8b1ede96775
kaikerodrigues835-collab <kaikerodrigues835@gmail.com>
2026-09-11 13:52:12 -0300

    Update SmoothScroll.tsx

--- src/components/shared/SmoothScroll.tsx
+++ src/components/shared/SmoothScroll.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReactNode, useEffect } from "react";
-import Lenis from "lenis";
+import Lenis from "@studio-freight/lenis";
 
 interface SmoothScrollProps {
   children: ReactNode;
